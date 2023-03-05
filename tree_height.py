d66521e9d70cc6c3fbfe7cbaef43c5745221aeea
Artūrs Adrians Zaharovs - Ploriņš <118048111+GingerTallMan@users.noreply.github.com>
2023-03-05 18:53:47 +0000

    modified 221RDB254

--- tree_height.py
+++ tree_height.py
@@ -2,34 +2,48 @@
 # Imports
 import sys
 import threading
-import numpy
 
-# Function 
 def compute_height(n, parents):
-    # Loops in range of n
+    # izveido koku
+    child1 = [[] for _ in range(n)]
     for i in range(n):
-        depth=0
-        indicator=i
-        while indicator!=-1:
-            depth=depth+1
-            indicator=parents[indicator]
-        max_height=max(max_height,depth)
-    return max_height
-#Main
+        parent = parents[i]
+        if parent == -1:
+            root = i
+        else:
+            child1[parent].append(i)
+
+    # koka augstums 
+    def compute_depth(node):
+        if not child1[node]:
+            return 1
+        max_depth = 0
+        for child2 in child1[node]:
+            depth = compute_depth(child2)
+            max_depth = max(max_depth, depth)
+        return max_depth + 1
+
+    return compute_depth(root)
+
+
 def main():
-    teksts=str(input())
-    if "I" in teksts:
-        number=int(input())
-        lists=list(map(int, input().split()))
-        print(compute_height(number, lists))
-    if "F" in teksts:
-        vards=str(input())
-        vards="test/"+str(vards)
-        fails=open(vards,'r')
-        number=int(fails.readline())
-        lists=list(map(int, fails.readline().split()))
-        fails.close()
-        print(compute_height(number, lists))
+    input_type = input("Input type")
+    if 'I' in input_type:
+        temp = int(input())
+        temp2 = list(map(int, input().split()))
+        height = compute_height(temp, temp2)
+        print(height)
+    elif 'F' in input_type:
+        filename = input()
+        with open("test/" + filename, 'r') as file:
+            temp = int(file.readline())
+            temp2 = list(map(int, file.readline().split()))
+            height = compute_height(temp, temp2)
+            print(height)
+    else:
+        print("Invalid")
+        exit()
+
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
